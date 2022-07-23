f21cfba2ebd8a669580225b458f6c1cc36fafff0
Antonio <antoniosansotta@gmail.com>
2022-07-23 15:29:36 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Packages
+const express = require('express')
+require('dotenv').config()
+
+// Modules
+const connectDB = require('./config')
+
+// Variables
+const PORT = process.env.PORT
+
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.ejs')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
