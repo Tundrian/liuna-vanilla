5c6b1e835c8d3f6b1a8b9f38598e40ac2ed3967a
Tundrian <antoniosansotta@gmail.com>
2022-07-29 11:13:58 -0400

    validated fetching all members on front end

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,15 @@
-const members = []
-
 // Functions
 console.log('file')
 const getMembers = async() => {
     console.log('here')
     const response = await fetch('../api/member')
     const membersList = await response.json()
-    members = [...membersList]
+    const list = document.querySelector('.data-view')
+    membersList.forEach(member => {
+        const li = document.createElement('li')
+        li.innerText = member.name
+        list.appendChild(li)
+    })
 }
 
 // Event Listeners
