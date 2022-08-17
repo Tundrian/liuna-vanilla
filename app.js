d21c39f2ecb41df7eba77ea8024c9986e0896346
Antonio <antoniosansotta@gmail.com>
2022-08-16 21:31:13 -0400

    added user auth views, routes, and validate functionality

--- app.js
+++ app.js
@@ -54,13 +54,13 @@ const mainRoutes = require('./routes/main')
 const apiRoutes = require('./routes/apiRoutes')
 const adminRoutes = require('./routes/adminRoutes')
 const memberRoutes = require('./routes/memberRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 app.use('/', mainRoutes)
+app.use('/auth', authRoutes)
 app.use('/api', apiRoutes)
 app.use('/admin', adminRoutes)
 app.use('/member', memberRoutes)
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running at `.blue + `http://localhost:${PORT}`.underline.cyan)
--- routes/main.js
+++ routes/main.js
@@ -1,92 +1,95 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
-const passport = require('passport')
+// const bcrypt = require('bcrypt')
+// const User = require('../models/User')
+// const passport = require('passport')
 
 router.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-// Register Handle
-router.post('/register', (req, res) => {
-    const { name, email, password, cpassword} = req.body
-    let errors = []
-    // Check required fields
-    if(!name || !email || !password || !cpassword){
-        errors.push({ msg: 'Please fill in all fields'})
-    }
 
-    // Check passwords match
-    if(password !== cpassword){
-        errors.push({ msg: 'Passwords do not match'})
-    }
 
-    // Check pass length
-    if(password.length < 6){
-        errors.push({ msg: 'Password should be at least 6 characters'})
-    }
+// // Register Handle
+// router.post('/register', (req, res) => {
+//     console.log('register')
+//     const { name, email, password, cpassword} = req.body
+//     let errors = []
+//     // Check required fields
+//     if(!name || !email || !password || !cpassword){
+//         errors.push({ msg: 'Please fill in all fields'})
+//     }
 
-    if(errors.length > 0){
-        res.render('index', { 
-            errors,
-            name,
-            email,
-            password,
-            cpassword
-        })
-    }else {
-        // validation passed
-        User.findOne({ email: email})
-        .then(user => {
-            if(user){
-                //user exists
-                errors.push({ msg: 'Email is already registered'})
-                res.render('index', { 
-                    errors,
-                    name,
-                    email,
-                    password,
-                    cpassword
-                })
-            } else {
-                const newUser = new User({
-                    name: name.toLowerCase(),
-                    email: email.toLowerCase(),
-                    password
-                })
+//     // Check passwords match
+//     if(password !== cpassword){
+//         errors.push({ msg: 'Passwords do not match'})
+//     }
 
-                // Hash Password
-                bcrypt.genSalt(10, (err, salt) => 
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) throw err
+//     // Check pass length
+//     if(password.length < 6){
+//         errors.push({ msg: 'Password should be at least 6 characters'})
+//     }
 
-                        // Set password to hashed
-                        newUser.password = hash
+//     if(errors.length > 0){
+//         res.render('index', { 
+//             errors,
+//             name,
+//             email,
+//             password,
+//             cpassword
+//         })
+//     }else {
+//         // validation passed
+//         User.findOne({ email: email})
+//         .then(user => {
+//             if(user){
+//                 //user exists
+//                 errors.push({ msg: 'Email is already registered'})
+//                 res.render('index', { 
+//                     errors,
+//                     name,
+//                     email,
+//                     password,
+//                     cpassword
+//                 })
+//             } else {
+//                 const newUser = new User({
+//                     name: name.toLowerCase(),
+//                     email: email.toLowerCase(),
+//                     password
+//                 })
 
-                        // Save user
-                        newUser.save()
-                        .then(user => {
-                            req.flash('success_msg', 'You are now registered and can log in')
-                            // res.redirect('/admin')
-                            res.redirect('/')
-                        })
-                        .catch(err => console.error(err))
-                }))
+//                 // Hash Password
+//                 bcrypt.genSalt(10, (err, salt) => 
+//                     bcrypt.hash(newUser.password, salt, (err, hash) => {
+//                         if(err) throw err
 
-            }
-        })
-    }
-})
+//                         // Set password to hashed
+//                         newUser.password = hash
 
-// Login Handle
-router.post('/login', (req, res, next) => {
-    console.log(req.body)
-    passport.authenticate('local', {
-        successRedirect: '/admin',
-        failureRedirect: '/',
-        failureFlash: true
-    })(req, res, next)
-})
+//                         // Save user
+//                         newUser.save()
+//                         .then(user => {
+//                             req.flash('success_msg', 'You are now registered and can log in')
+//                             // res.redirect('/admin')
+//                             res.redirect('/')
+//                         })
+//                         .catch(err => console.error(err))
+//                 }))
+
+//             }
+//         })
+//     }
+// })
+
+// // Login Handle
+// router.post('/login', (req, res, next) => {
+//     console.log(req.body)
+//     passport.authenticate('local', {
+//         successRedirect: '/admin',
+//         failureRedirect: '/',
+//         failureFlash: true
+//     })(req, res, next)
+// })
 
 module.exports = router
