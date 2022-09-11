44ae26aef691559e2bbd6d9cf09b9d3f86f88945
Antonio <antoniosansotta@gmail.com>
2022-09-11 17:39:19 -0400

    added authenticated routes for the admin section

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,15 +2,15 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.route('/', ensureAuth).get((req,res) => res.render('admin/dashboard.ejs'))
+router.route('/').get(ensureAuth, (req,res) => res.render('admin/dashboard.ejs'))
 
 /* Admin Routes */
-router.route('/member').get((req, res) =>  res.render('admin/member/index.ejs'))
-router.route('/contractor').get((req, res) => res.render('admin/contractor/index.ejs'))
-router.route('/certificate').get((req, res) => res.render('admin/certificate/index.ejs'))
-router.route('/course', ensureAuth).get((req, res) =>  res.render('admin/course/index.ejs'))
-router.route('/dispatch', ensureAuth).get((req, res) =>  res.render('admin/dispatch/index.ejs'))
-router.route('/trainingSession', ensureAuth).get((req, res) =>  res.render('admin/trainingSession/index.ejs'))
-router.route('/user', ensureAuth).get((req, res) =>  res.render('admin/user/index.ejs'))
+router.route('/member').get(ensureAuth, (req, res) =>  res.render('admin/member/index.ejs'))
+router.route('/contractor').get(ensureAuth, (req, res) => res.render('admin/contractor/index.ejs'))
+router.route('/certificate').get(ensureAuth, (req, res) => res.render('admin/certificate/index.ejs'))
+router.route('/course', ensureAuth).get(ensureAuth, (req, res) =>  res.render('admin/course/index.ejs'))
+router.route('/dispatch', ensureAuth).get(ensureAuth, (req, res) =>  res.render('admin/dispatch/index.ejs'))
+router.route('/trainingSession', ensureAuth).get(ensureAuth, (req, res) =>  res.render('admin/trainingSession/index.ejs'))
+router.route('/user', ensureAuth).get(ensureAuth, (req, res) => res.render('admin/user/index.ejs'))
 
 module.exports = router
