648998e02191b852d659b120c2c1589b029a0a6a
Antonio <antoniosansotta@gmail.com>
2022-09-24 09:40:59 -0400

    upadted styling on the certificate page and the logic on the certificate js to use tailwind

--- public/js/certificate.js
+++ public/js/certificate.js
@@ -9,7 +9,7 @@ let dataSelected = {
     expirationUnits: '',
 }
 let formFields = {
-    container: document.querySelector('.view-modal-container'),
+    // container: document.querySelector('.view-modal-container'),
     name: document.querySelector('#view-name'),
     description: document.querySelector('#view-description'),
     type: document.querySelector('#view-type'),
@@ -21,7 +21,7 @@ const fetchUri = `../api/certificate`
 
 const closeModal = () => {
     
-    formFields.container.classList.add('hide')
+    document.querySelector('#fetch-add-btn').innerText = 'ADD'
 
     Object.keys(formFields).forEach(key => {
         formFields[key].value = ''
@@ -31,7 +31,6 @@ const closeModal = () => {
         dataSelected[key] = null
     })
 }
-
 
 const openModal = async (e) => {
     if(e !== 'add'){
@@ -64,7 +63,7 @@ const openModal = async (e) => {
         document.querySelector('#view-type').appendChild(op)
     })
     
-    formFields.container.classList.remove('hide');
+    // formFields.container.classList.remove('hide');
 }
 
 const getData = async (id) => {
@@ -90,34 +89,62 @@ const getDatas = async() => {
     const dataList = await response.json()
     const list = document.querySelector('.fetch-view-results')
 
-    dataList.forEach(data => {
-        const liContainer = document.createElement('li')
-        const ul = document.createElement('ul')
-        const liViewBtn = document.createElement('button')
-        const liView = document.createElement('li')
-        let lis = []
+    // dataList.forEach(data => {
+    //     const liContainer = document.createElement('li')
+    //     const ul = document.createElement('ul')
+    //     const liViewBtn = document.createElement('button')
+    //     const liView = document.createElement('li')
+    //     let lis = []
 
+    //     Object.keys(formFields).forEach((key, i) => {
+    //         lis.push(document.createElement('li'))
+    //         lis[i].innerText = data[key]
+    //     }) 
+        
+    //     lis.shift()
+        
+    //     liViewBtn.innerText = 'VIEW'
+    //     liViewBtn.classList.add('view-btn', 'btn')
+    //     liContainer.classList.add('view-results-list')
+    //     liViewBtn.setAttribute('data-id', data._id)
+        
+    //     liView.appendChild(liViewBtn)
+
+    //     ul.classList.add('table-body')
+
+    //     lis.forEach(li => ul.appendChild(li))
+        
+    //     ul.appendChild(liView)
+    //     liContainer.appendChild(ul)
+    //     list.appendChild(liContainer)
+    // })
+    dataList.forEach((data, i) => {
+        const tr = document.createElement('tr')
+        const liViewBtn = document.createElement('label')
+        const liView = document.createElement('td')
+        let lis = []
         Object.keys(formFields).forEach((key, i) => {
-            lis.push(document.createElement('li'))
+            lis.push(document.createElement('td'))
             lis[i].innerText = data[key]
         }) 
-        
-        lis.shift()
-        
+
+        tr.classList.add('view-results-list')
+
         liViewBtn.innerText = 'VIEW'
-        liViewBtn.classList.add('view-btn', 'btn')
-        liContainer.classList.add('view-results-list')
+        liViewBtn.classList.add('view-btn', 'btn', 'modal-button', 'text-md')
         liViewBtn.setAttribute('data-id', data._id)
+        liViewBtn.setAttribute('for', 'modal')
         
         liView.appendChild(liViewBtn)
-
-        ul.classList.add('table-body')
-
-        lis.forEach(li => ul.appendChild(li))
         
-        ul.appendChild(liView)
-        liContainer.appendChild(ul)
-        list.appendChild(liContainer)
+        if(i % 2 !== 0){
+            tr.classList.add('active')
+        }
+
+        lis.forEach(li => tr.appendChild(li))
+        
+        tr.appendChild(liView)
+        list.appendChild(tr)
     })
 
     // Add event listeners to all view buttons
@@ -177,8 +204,8 @@ const enableEdit = (e) => {
         formFields[key].disabled = false
     })
     
-    document.querySelector('.view-edit-button').classList.add('hide')
-    document.querySelector('.view-edit-confirm-btn').classList.remove('hide')
+    document.querySelector('.view-edit-button').classList.add('hidden')
+    document.querySelector('.view-edit-confirm-btn').classList.remove('hidden')
     document.querySelector('.view-edit-confirm-btn').disabled = false
     document.querySelector('.view-edit-button').disabled = true
 }
@@ -186,7 +213,7 @@ const enableEdit = (e) => {
 const editData = async(e) => {
     e.preventDefault()
 
-    if(document.querySelector('.view-edit-confirm-btn').classList.contains('hide')){
+    if(document.querySelector('.view-edit-confirm-btn').classList.contains('hidden')){
         return
     }
 
@@ -210,10 +237,10 @@ const editData = async(e) => {
         formFields[key].disabled = true
       })
 
-    document.querySelector('.view-edit-button').classList.remove('hide')
-    document.querySelector('.view-edit-confirm-btn').classList.add('hide')
-    document.querySelector('.view-edit-confirm-btn').disabled = true
-    document.querySelector('.view-edit-button').disabled = false
+      document.querySelector('.view-edit-button').classList.remove('hidden')
+      document.querySelector('.view-edit-confirm-btn').classList.add('hidden')
+      document.querySelector('.view-edit-confirm-btn').disabled = true
+      document.querySelector('.view-edit-button').disabled = false
     return response.json()
 }
 
@@ -231,23 +258,28 @@ const addClicked = async(e) => {
 
 const handleModal = (type) => {
     if(type.toLowerCase() === 'add'){
-        document.querySelector('.form-submit').classList.remove('hide')
-        document.querySelector('.edit-btn').classList.add('hide')
-        document.querySelector('.edit-confirm-btn').classList.add('hide')
-        document.querySelector('.delete-btn').classList.add('hide')
+        document.querySelector('#form-submit').classList.remove('hidden')
+        document.querySelector('.edit-btn').classList.add('hidden')
+        document.querySelector('.edit-confirm-btn').classList.add('hidden')
+        document.querySelector('.delete-btn').classList.add('hidden')
     }else {
-        document.querySelector('.form-submit').classList.add('hide')
-        document.querySelector('.edit-btn').classList.remove('hide')
-        document.querySelector('.edit-confirm-btn').classList.add('hide')
-        document.querySelector('.delete-btn').classList.remove('hide')
+        document.querySelector('#form-submit').classList.add('hidden')
+        document.querySelector('.edit-btn').classList.remove('hidden')
+        document.querySelector('.edit-confirm-btn').classList.add('hidden')
+        document.querySelector('.delete-btn').classList.remove('hidden')
+
+        document.querySelector('.view-edit-button').classList.remove('hidden')
+        document.querySelector('.view-edit-confirm-btn').classList.add('hidden')
+        document.querySelector('.view-edit-confirm-btn').disabled = true
+        document.querySelector('.view-edit-button').disabled = false
     }
 }
 
 // Event Listeners
 document.querySelector('.fetch-view-btn').addEventListener('click', getDatas)
 document.querySelector('#fetch-add-btn').addEventListener('click', addClicked)
-document.querySelector('.form-submit').addEventListener('click', addData)
-document.querySelector('.view-modal-close-btn').addEventListener('click', closeModal)
+document.querySelector('#form-submit').addEventListener('click', addData)
+document.querySelector('#view-modal-close-btn').addEventListener('click', closeModal)
 document.querySelector('.view-delete-button').addEventListener('click', deleteData)
 document.querySelector('.view-edit-button').addEventListener('click', enableEdit)
 document.querySelector('.view-edit-confirm-btn').addEventListener('click', editData)
